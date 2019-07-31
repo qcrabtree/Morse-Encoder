439458f376278510f1fae50c29aec709f8d326c7
Quinn <qcrabtree23@gmail.com>
2019-07-31 16:22:57 -0400

    first commit

---
+++ js/morse.js
@@ -0,0 +1,23 @@
+function convert(){
+
+
+
+let letters = [ ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' ];
+let morseLetters = [ "\xa0\xa0\xa0\xa0", ". ___", "___ . . .", "___ . ___ .", "___ . .", ".", ". . ___ .", "___ ___ .", ". . . .", ". .", ". ___ ___ ___", "___ . ___", ". ___ . .", "___ ___", "___ .", "___ ___ ___", ". ___ ___ .", "___ ___ . ___", ". ___ .", ". . .", "_", ". . ___", ". . . ___", ". ___ ___", "___ . . ___", "___ . ___ ___", "___ ___ . .", ". ___ ___ ___ ___", ". . ___ ___ ___", ". . . ___ ___", ". . . . ___", ". . . . .", "___ . . . .", "___ ___ . . .", "___ ___ ___ . .", "___ ___ ___ ___ .", "___ ___ ___ ___ ___" ];
+
+
+let textToChange = document.getElementById("textToConvert").value;
+
+let newText = "";
+
+for ( let i = 0; i < textToChange.length; i++) {
+    for ( let x = 0; x < 37; x++) {
+        if (textToChange[i].toLowerCase() == letters[x]) {
+            newText += morseLetters[x];
+            newText += "\xa0\xa0\xa0\xa0";
+            break;
+        }
+    }
+}
+document.getElementById("newText").innerHTML = newText;
+}
